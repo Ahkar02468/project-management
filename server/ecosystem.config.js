78bf77b6c2d54415881239d8655e794989042d56
Ahkar02468 <arkarshwebaw@gmail.com>
2024-10-25 17:48:46 +0700

    server ecosystem modify

---
+++ server/ecosystem.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+     apps: [
+          {
+               name: "project-management",
+               script: "pnpm",
+               args: "dev",
+               env: {
+                    NODE_ENV: "development"
+               }
+          }
+     ]
+}

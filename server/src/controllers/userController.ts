f02790b38f742d88cc69cd2dc634b75adf383e2c
Ahkar02468 <arkarshwebaw@gmail.com>
2024-10-26 15:52:54 +0700

    create post user route and controller

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -15,3 +15,28 @@ export const getUsers = async (
                .json({ message: `Error performing fetching users ${error.message}` })
      }
 }
+
+export const postUser = async (req: Request, res: Response) => {
+     const {
+          username,
+          cognitoId,
+          profilePictureUrl = "i1.jpg",
+          teamId = 1,
+     } = req.body
+
+     try {
+          const newUsers = await prisma.user.create({
+               data: {
+                    username,
+                    cognitoId,
+                    profilePictureUrl,
+                    teamId
+               }
+          })
+          res.json({ message: "User Created Successfully", newUsers })
+     } catch (error: any) {
+          res
+               .status(500)
+               .json({ message: `Error performing fetching users ${error.message}` })
+     }
+}

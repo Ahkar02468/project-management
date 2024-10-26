9d15d4b955c769540d4a9e53fc72d8948a53ad3a
Ahkar02468 <arkarshwebaw@gmail.com>
2024-10-26 13:33:17 +0700

    "Update remotePatterns in next.config.mjs to an array with a single object"

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
      images: {
-          remotePatterns: {
-               protocol: 'https',
-               hostname: 'pm-sc-images.s3.us-east-1.amazonaws.com',
-               port: '',
-               pathname: '/**',
-          }
+          remotePatterns: [
+               {
+                    protocol: 'https',
+                    hostname: 'pm-sc-images.s3.us-east-1.amazonaws.com',
+                    port: '',
+                    pathname: '/**',
+               }
+          ]
      }
 };
 
